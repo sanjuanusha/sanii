e8d968e18a804549e9b4b12ea20dc0eaee1dd268
saniya <smsaniyaparveen786@gmail.com>
2024-07-22 12:34:12 +0530

    adding pgm1.java

---
+++ pgm1.java
@@ -0,0 +1,17 @@
+class sani
+{
+	public static void main(String s[])
+	{
+		int [][]a={{10,20,30},{40,50,60},{70,80,90}};
+		for(int i=0;i<=2;i++)
+		{
+			for(int j=0;j<=2;j++)
+			{
+				System.out.println(a[i][j]);
+				System.out.print(" ");
+			}
+			System.out.println("\n");
+		}
+	}
+	
+}
